034deaad09c6ac85360019233b88f780b28bde04
wenrunyuan <1078253825@qq.com>
2023-04-30 13:10:51 +0800

    DI_set

---
+++ spring05_DI_set/src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -0,0 +1,27 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.BookDao;
+import com.itheima.dao.UserDao;
+import com.itheima.service.BookService;
+
+public class BookServiceImpl implements BookService {
+    //5. 删除业务层中使用new的方式创建的 dao 对象
+    private BookDao bookDao;
+    private UserDao userDao;
+
+
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    public void save() {
+        System.out.println("book service save ...");
+        bookDao.save();
+        userDao.save();
+    }
+}
