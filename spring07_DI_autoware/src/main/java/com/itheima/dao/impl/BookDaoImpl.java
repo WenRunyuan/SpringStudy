041a28a3be1f51dce162d82b324d56284edf17a0
wenrunyuan <1078253825@qq.com>
2023-04-30 16:00:16 +0800

    DI_autoware

---
+++ spring07_DI_autoware/src/main/java/com/itheima/dao/impl/BookDaoImpl.java
@@ -0,0 +1,12 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.BookDao;
+
+import java.util.PrimitiveIterator;
+
+public class BookDaoImpl implements BookDao {
+
+    public void save() {
+        System.out.println("book dao save...");
+    }
+}
