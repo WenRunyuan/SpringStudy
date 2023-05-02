b6979fe82ceec71123c49fec53f1d0e9415dcf49
wenrunyuan <1078253825@qq.com>
2023-05-02 20:43:34 +0800

    annotation_bean_manager

---
+++ spring12_annotation_bean_manager/src/main/java/com/itheima/config/SpringConfig.java
@@ -0,0 +1,12 @@
+package com.itheima.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan("com.itheima")
+
+public class SpringConfig {
+
+}
