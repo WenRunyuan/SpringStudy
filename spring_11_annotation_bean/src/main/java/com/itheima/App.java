7df6b5f48c24a586a0c7c698338bf341b3a17d93
wenrunyuan <1078253825@qq.com>
2023-05-02 17:59:42 +0800

    annotation_bean

---
+++ spring_11_annotation_bean/src/main/java/com/itheima/App.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import com.itheima.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+//        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+//        bookDao.save();
+
+        BookService bookService = (BookService) ctx.getBean(BookService.class);
+        bookService.save();
+    }
+}
