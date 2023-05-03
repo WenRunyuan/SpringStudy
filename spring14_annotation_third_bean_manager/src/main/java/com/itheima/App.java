dde369eb94a276420f9ffae0812031fa86c856f2
wenrunyuan <1078253825@qq.com>
2023-05-03 19:34:17 +0800

    annotation_thirdbean_manager

---
+++ spring14_annotation_third_bean_manager/src/main/java/com/itheima/App.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+import com.itheima.config.SpringConfig;
+import com.itheima.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        DataSource dataSource = ctx.getBean(DataSource.class);
+        System.out.println(dataSource);
+    }
+}
