a89c6f55f663a76244afc1561d8f3440977455a0
wenrunyuan <1078253825@qq.com>
2023-05-03 18:10:28 +0800

    annotation_DI

---
+++ spring13_annotation_di/src/main/java/com/itheima/App.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import com.itheima.config.SpringConfig;
+import com.itheima.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        BookService bookService = (BookService) ctx.getBean(BookService.class);
+        bookService.save();
+    }
+}
---
+++ spring13_annotation_di/src/main/java/com/itheima/dao/impl/BookDaoImpl.java
@@ -0,0 +1,17 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.BookDao;
+import com.itheima.service.BookService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository("bookDao")
+
+public class BookDaoImpl implements BookDao {
+    @Value("${name}")
+    private String name;
+    public void save() {
+        System.out.println("book dao save..." + name);
+    }
+}
